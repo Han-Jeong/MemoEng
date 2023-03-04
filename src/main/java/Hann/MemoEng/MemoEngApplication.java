177e194ae48b5bb15c61c3db0ad24fdf7bbc587d
hann <wjdgks1233@gmail.com>
2023-03-04 15:22:44 +0900

    first commit

---
+++ src/main/java/Hann/MemoEng/MemoEngApplication.java
@@ -0,0 +1,13 @@
+package Hann.MemoEng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoEngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoEngApplication.class, args);
+	}
+
+}
